58486d55d3844b3a7323331b8a5ccc6ffdaedfd8
TK Soh <teekaysoh@gmail.com>
2024-05-20 11:31:37 +0800

    add static method to draw pattern

--- lib/geopattern_flutter.dart
+++ lib/geopattern_flutter.dart
@@ -14,13 +14,17 @@ class GeoPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawColor(background, BlendMode.color);
+    drawPattern(canvas, pattern, size);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+
+  static drawPattern(Canvas canvas, GeoPattern pattern, Size size) {
     for (var i = 0.0; i < size.height; i += pattern.size.height) {
       for (var j = 0.0; j < size.width; j += pattern.size.width) {
         pattern.paint(canvas, Offset(j, i));
       }
     }
   }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
