29de5530784c3b34d8e19c078c59eb195bfce7a9
TK Soh <teekaysoh@gmail.com>
2024-05-20 13:31:21 +0800

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:geopattern_flutter/geopattern_flutter.dart';
 import 'package:geopattern_flutter/patterns/overlapping_circles.dart';
+import 'package:geopattern_flutter/patterns/pattern.dart';
 
 void main() => runApp(const App());
 
@@ -40,16 +41,26 @@ class App extends StatelessWidget {
           ),
           SliverList(
               delegate: SliverChildListDelegate(<Widget>[
-            Card(child: Container(height: 200)),
-            Card(child: Container(height: 200)),
-            Card(child: Container(height: 200)),
-            Card(child: Container(height: 200)),
-            Card(child: Container(height: 200)),
-            Card(child: Container(height: 200)),
+            buildDecoratedCard(Colors.red),
+            buildDecoratedCard(Colors.blue),
+            buildDecoratedCard(Colors.green),
+            buildDecoratedCard(Colors.cyan),
+            buildDecoratedCard(Colors.teal),
+            buildDecoratedCard(Colors.orange),
           ]))
         ],
       ),
     ));
+  }
+
+  Widget buildDecoratedCard(Color background) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Container(
+        height: 200,
+        decoration: CustomDecoration(background: background),
+      ),
+    );
   }
 }
 
@@ -75,5 +86,44 @@ class AppBarBackground extends StatelessWidget {
           size: Size(constraints.maxWidth, constraints.maxHeight),
           painter: GeoPainter(pattern: pattern, background: Colors.yellow));
     });
+  }
+}
+
+class CustomDecoration extends Decoration {
+  final Color? background;
+
+  const CustomDecoration({this.background});
+
+  @override
+  BoxPainter createBoxPainter([void Function()? onChanged]) {
+    final gen = Random();
+    final pattern = OverlappingCircles(
+        radius: 60,
+        nx: 6,
+        ny: 6,
+        fillColors: List.generate(
+            36,
+            (int i) => Color.fromARGB(
+                10 + (gen.nextDouble() * 100).round(),
+                50 + gen.nextInt(2) * 150,
+                50 + gen.nextInt(2) * 150,
+                50 + gen.nextInt(2) * 150)));
+    return BoxDecorationPainter(pattern: pattern, background: background);
+  }
+}
+
+class BoxDecorationPainter extends BoxPainter {
+  final Color? background;
+  GeoPattern pattern;
+  BoxDecorationPainter({required this.pattern, this.background});
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final size = configuration.size!;
+    if (background != null) {
+      canvas.drawColor(background!, BlendMode.srcOver);
+    }
+
+    GeoPainter.drawPattern(canvas, pattern, size);
   }
 }
