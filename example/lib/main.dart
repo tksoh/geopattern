2f3f2ba292f602cc5d2daa29bee6c7b6583498d8
TK Soh <teekaysoh@gmail.com>
2024-05-22 14:22:52 +0800

    remove unused custom appbar class in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:geopattern_flutter/geopattern_flutter.dart';
 import 'package:geopattern_flutter/patterns/chevrons.dart';
@@ -67,30 +65,5 @@ class App extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class AppBarBackground extends StatelessWidget {
-  const AppBarBackground({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      final gen = Random();
-      final pattern = OverlappingCircles(
-          radius: 60,
-          nx: 6,
-          ny: 6,
-          fillColors: List.generate(
-              36,
-              (int i) => Color.fromARGB(
-                  10 + (gen.nextDouble() * 100).round(),
-                  50 + gen.nextInt(2) * 150,
-                  50 + gen.nextInt(2) * 150,
-                  50 + gen.nextInt(2) * 150)));
-      return CustomPaint(
-          size: Size(constraints.maxWidth, constraints.maxHeight),
-          painter: GeoPainter(pattern: pattern, background: Colors.yellow));
-    });
   }
 }
